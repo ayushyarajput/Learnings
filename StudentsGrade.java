d3cb1289ea509e2de0fff06f3d7b427717028adb
Ayushya Nirwan <nayushya@gmail.com>
2023-04-16 23:22:30 +0530

    added gitignore file and comments in students grade file

--- StudentsGrade.java
+++ StudentsGrade.java
@@ -2,9 +2,16 @@ package Learnings;
 import java.util.Scanner;
 public class StudentsGrade {
     public static void main (String args[]){
+
         try (Scanner myObj = new Scanner(System.in)) {
+            // taking input from user
             System.out.print("Enter Marks : ");
             int marks = myObj.nextInt();
+            // if marks is greater than 100 - it is not possible 
+            // if greater than 90 than A grade
+            // if greater than 80 than B grade
+            // if greater than 70 than C greade 
+            // below that person eill need more practice
             if(marks>100){
                 System.out.println("Please enter acceptable marks");
             } else if(marks>90){
@@ -16,6 +23,7 @@ public class StudentsGrade {
             }else {
                 System.out.println("Yo need to practice more and work hard");
             }
+            // exception handling if user enters any other value than integer marks
         } catch (Exception e) {
             System.out.println("you might have entered wrong values");
         }
